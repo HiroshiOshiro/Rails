0e73a786157a1623c62822ccbb6d70426e87f9fd
hiroshi oshiro <ooshiro010@gmail.com>
2020-05-16 00:19:51 +0900

    8-2 javascript をサーバから発行

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     # redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
-    head :no_content
+    # head :no_content
   end
 
   def create
